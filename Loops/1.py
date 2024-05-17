c72d123e49267d22f760af3c78e8dd75f60da2dc
Divyansh <85442869+divyaannsh@users.noreply.github.com>
2024-05-17 13:17:35 +0530

    Loops🔥

---
+++ Loops/1.py
@@ -0,0 +1,10 @@
+numbers = [1,2,3,4,5,5,6,6,7,7,8,8,9,9]
+
+
+positive_number_count =0
+
+for num in numbers:
+ if num > 0:
+    positive_number_count+= 1
+
+    print("Final Count OF postitve numbe is :",positive_number_count)
---
+++ Loops/4.py
@@ -0,0 +1,6 @@
+input_str = "Divyansh Coder"
+reversed_str =""
+
+for Char in input_str:
+ reversed_str = reversed_str +Char
+ print(reversed_str)
